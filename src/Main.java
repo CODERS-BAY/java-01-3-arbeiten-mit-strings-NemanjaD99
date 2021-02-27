4ef11b57acc42a22e5f9d0b61e94a188c16e3911
Nemanja <nemanjadragomirovic086@gmail.com>
2021-02-27 11:18:11 +0100

    Implement Java 1.3. Arbeiten mit Strings

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String text=" Hello World";
+        String text2="Hello CodersBay";
+        System.out.println(text);
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.replace(text, text2));
+
+    }
+}
